a4c30ae2fc93d9ead5b2aa5b568ff6b3e8cb6838
Btaft6278 <btaft6278@gmail.com>
2022-05-25 17:24:33 -0500

    eod

--- src/components/GetArray.js
+++ src/components/GetArray.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import style from "../css/newArray.module.css";
 
 //****************** NEW ARRAY ********************* */
